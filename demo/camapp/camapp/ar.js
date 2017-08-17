b8211f3709a92df39cc8cba9f98b42009b3d051a
Andrey Pashentsev <avp200681@gmail.com>
2017-08-17 19:15:13 +0300

    test

--- demo/camapp/camapp/ar.js
+++ demo/camapp/camapp/ar.js
@@ -76,11 +76,9 @@ class AR {
     }
     
     toggleDebug(isDebug) {
-        if (window.webkit.messageHandlers.showDebug) {
-            window.webkit.messageHandlers.showDebug.postMessage({
-                debug: isDebug ? 1 : 0
-            });
-        }
+        window.webkit.messageHandlers.showDebug.postMessage({
+            debug: isDebug ? 1 : 0
+        });
     }
     
     watch(options, callback) {
