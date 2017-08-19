034b108c8f8b19cfab2a5edbf3878350f69d2021
Andrey Pashentsev <avp200681@gmail.com>
2017-08-19 13:47:46 +0300

    test

--- demo/camapp/camapp/app-three-sync.js
+++ demo/camapp/camapp/app-three-sync.js
@@ -384,7 +384,7 @@ class App {
         });
         document.querySelector('#info-location').value = 
             'Camera:' + JSON.stringify(this.camera.getWorldPosition()) + "\n---\n" +
-            'S:' + JSON.stringify({
+            'Si:' + JSON.stringify({
                 w: window.innerWidth, h: window.innerHeight, a: window.innerWidth / window.innerHeight,
                 sw: screen.width, sh: screen.height, sa: screen.width / screen.height
             })
