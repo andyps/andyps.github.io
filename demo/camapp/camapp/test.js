dfbc10d3729f654596fb2dc51803bbb57246b1a5
Andrey Pashentsev <avp200681@gmail.com>
2017-10-19 16:36:28 +0300

    test

--- demo/camapp/camapp/test.js
+++ demo/camapp/camapp/test.js
@@ -98,7 +98,7 @@ class App {
 
         this.ar.addEventListener(ARKitWrapper.SIZE_CHANGED_EVENT, (e) => {
             this.addMessage('resized:' + JSON.stringify(e.detail));
-            this.resize(e.detail.size.width, e.detail.size.height);
+            //this.resize(e.detail.size.width, e.detail.size.height);
         });
 
         this.ar.addEventListener(ARKitWrapper.PLAINS_ADDED_EVENT, (e) => {
@@ -400,7 +400,7 @@ class App {
     }
     
     onARInit(e) {
-        this.showMessage('init!' + JSON.stringify(e));
+        this.showMessage('init!!!' + JSON.stringify(e));
         if (!this.ar.deviceInfo || !this.ar.deviceInfo.uuid) {
             return;
         }
