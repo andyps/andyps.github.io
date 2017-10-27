cfbb0290348f788f7cb8330cd518a9c5d400c005
Andrey Pashentsev <avp200681@gmail.com>
2017-10-27 10:25:53 +0300

    test

---
+++ demo/camapp/camapp/graffity/EditControls.js
@@ -0,0 +1,88 @@
+import EventHandlerBase from '../fill/EventHandlerBase.js'
+
+export default class EditControls extends EventHandlerBase {
+    constructor(app) {
+        super();
+        this.app = app;
+        this.canvas = app.canvas;
+        
+        this.touches = null;
+        this.doubleTapPossible = false;
+        
+        this.canvas.addEventListener('touchstart', e => {
+            this.onTouchStart(e);
+        }, false);
+        this.canvas.addEventListener('touchmove', e => {
+            this.onTouchMove(e);
+        }, false);
+        this.canvas.addEventListener('touchend', e => {
+            this.onTouchEnd(e);
+        }, false);
+        this.canvas.addEventListener('touchcancel', e => {
+            this.onTouchCancel(e);
+        }, false);
+        
+    }
+}
+
+EditControls.EVENT_DOUBLETAP = 'double-tap';
+
+/*
+import EventHandlerBase from '../fill/EventHandlerBase.js'
+
+export default class EditControls extends EventHandlerBase {
+    constructor(canvas) {
+        super();
+        this.canvas = canvas;
+        
+        this.touches = null;
+        this.doubleTapPossible = false;
+        
+        this.canvas.addEventListener('touchstart', e => {
+            this.onTouchStart(e);
+        }, false);
+        this.canvas.addEventListener('touchmove', e => {
+            this.onTouchMove(e);
+        }, false);
+        this.canvas.addEventListener('touchend', e => {
+            this.onTouchEnd(e);
+        }, false);
+        this.canvas.addEventListener('touchcancel', e => {
+            this.onTouchCancel(e);
+        }, false);
+        
+    }
+    copyTouch(touch) {
+        return {
+            identifier: touch.identifier,
+            clientX: touch.clientX,
+            clientY: touch.clientY,
+            ts: 
+        }
+    }
+    onTouchStart(e) {
+        e.preventDefault();
+        
+        if (e.touches.length == 1 && !this.touches) {
+            // double tap is possible only when there is one touch
+            this.doubleTapPossible = true;
+        }
+        
+        
+    }
+    onTouchMove(e) {
+        e.preventDefault();
+    }
+    onTouchEnd(e) {
+        e.preventDefault();
+    }
+    onTouchCancel(e) {
+        e.preventDefault();
+    }
+    
+    
+    
+}
+
+EditControls.EVENT_DOUBLETAP = 'double-tap';
+*/
